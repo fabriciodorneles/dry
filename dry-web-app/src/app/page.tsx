50d050f5e9dcdcccbafea2a12b2116594d55e047
Fabricio Dorneles <fabricio.dorneles-contractor@procore.com>
2024-12-17 13:55:32 -0300

    initial commit

---
+++ dry-web-app/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Component } from "@/components/mainChart/Chart";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      DRY - Weight tracker for geeks
+      <Component />
+    </div>
+  );
+}
